6e702617d2579702d22daa596e84cff1a17a0422
Xiang511 <toby91511@gmail.com>
2025-11-30 01:24:36 +0800

    fix: 修復 SiteStates 樣式

--- server/src/public/js/datatable.js
+++ server/src/public/js/datatable.js
@@ -37,3 +37,23 @@ $(document).ready(function () {
         }
     });
 });
+
+//即時查詢總人數
+const firstSpan = document.querySelector('.sitestatesJs');
+//取得字串
+const text = firstSpan.textContent;
+//取得數字
+const Real_Time_Number = text.substring(text.length - 5);
+
+
+//今日訪客
+const spans = document.querySelectorAll(".sitestatesJs");
+const secondSpan = spans[1];
+const secondSpan_txt = secondSpan.textContent;
+const secondSpan_result = secondSpan_txt.substring(text.length - 6);
+
+//線上人數
+const online = document.querySelectorAll(".sitestatesJs");
+const onlineSpan = online[2];
+const onlineSpan_txt = onlineSpan.textContent;
+const onlineSpan_result = onlineSpan_txt.substring(text.length - 4);
