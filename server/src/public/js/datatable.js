c8dfdc4ba331158d888c4cd440ca0829c45979c1
Xiang511 <toby91511@gmail.com>
2025-11-29 17:04:41 +0800

    refactor: 將js整合成一個檔案

--- server/src/public/js/datatable.js
+++ server/src/public/js/datatable.js
@@ -1,20 +1,39 @@
-let table = new DataTable('#myTable', {
-    order: [[2, 'asc']],
-    info: false,
-    // paging: false
-    processing: true,
-    // 表格拖拉
-    // colReorder: true,
-    // colReorder: {
-    //     fixedColumnsLeft: 1
-    // },
-    columnDefs: [{ orderable: false, targets: 0 }
-        // { "width": "25%", "targets": 0 },
-        // { "width": "5   %", "targets": [1,2,3,4] }
-    ]
-});
-table.on('order.dt search.dt', function () {
-    table.column(0, { search: 'applied', order: 'applied' }).nodes().each(function (cell, i) {
-        cell.innerHTML = i + 1;
+$(document).ready(function () {
+    let table = new DataTable('#myTable', {
+        order: [[2, 'desc']],
+        info: false,
+        processing: true,
+        colReorder: true,
+        colReorder: {
+            fixedColumnsLeft: 1
+        },
+        fixedColumns: {
+            start: 1,
+            end: 2
+        },
+        scrollCollapse: true,
+        language: {
+            processing: "處理中...",
+            search: "搜尋:",
+            lengthMenu: "_MENU_",
+            paginate: {
+                first: "第一頁",
+                last: "最後一頁",
+                next: "下一頁",
+                previous: "上一頁"
+            },
+            emptyTable: "目前沒有資料",
+            zeroRecords: "沒有符合的資料"
+        }
     });
-}).draw();
+
+    // 季度選擇器切換
+    $('#seasonSelect').on('change', function () {
+        const selectedSeason = $(this).val();
+        if (selectedSeason) {
+            window.location.href = `/players?time=${encodeURIComponent(selectedSeason)}`;
+        } else {
+            window.location.href = '/players';
+        }
+    });
+});
